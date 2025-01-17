89f04b51d447b265c27f2a51e3b780697e017abd
SAIKIRANghb <saikiransuguru@gmail.com>
2025-01-17 21:54:27 +0530

    updated app.py

--- app.py
+++ app.py
@@ -195,5 +195,9 @@ def play_uploaded_video(filename):
 def home():
     return render_template("index.html")
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
